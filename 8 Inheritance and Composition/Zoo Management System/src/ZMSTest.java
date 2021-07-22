b7437147a56a55f41f0cbeb719d58c42b072b9b8
Karan Singh Solanki <87369849+Metacube-KaranSingh-Solanki@users.noreply.github.com>
2021-07-22 16:49:28 +0530

    Update ZMSTest

--- 8 Inheritance and Composition/Zoo Management System/src/ZMSTest.java
+++ 8 Inheritance and Composition/Zoo Management System/src/ZMSTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -7,11 +8,12 @@ import org.junit.jupiter.api.Test;
 public class ZMSTest {
 	/** 2 Zones and 2 Cages each through out has been consider while adding the animal **/
 	ZooManagementSystem zms = new ZooManagementSystem(3);
+	Animal lion1 = new Lion(3, 40);			// For testing the death (removal) of animal 
 	
 	@BeforeEach
 	public void testCreateZoo() {
 		zms.setCapacity(2, 2);
-		zms.addToZoo(new Lion(3, 40)); //Zone1
+		zms.addToZoo(lion1); //Zone1
 		zms.addToZoo(new Crocodile(1, 10)); //Zone2
 		zms.addToZoo(new Lion(5, 45)); //Zone1
 		zms.addToZoo(new Crocodile(2, 15)); //Zone2
@@ -66,4 +68,21 @@ public class ZMSTest {
 		zms.addToZoo(new Peacock(5, 56));
 		assertFalse(zms.addToZoo(new Peacock(1, 95)));
 	}
-}
+	
+	/**
+	 * Removing an animal if it is present (On Animal Death)
+	 */
+	@Test
+	public void testRemoveLionPass() {
+		assertTrue(zms.deathOfAnimal(lion1.getId()));
+	}
+	
+	/**
+	 * Removing an animal if it is not present (On Animal Death)
+	 * Throws Assertion Error
+	 */
+	@Test
+	public void testRemoveLionFail() {
+		assertThrows(AssertionError.class, () -> {zms.deathOfAnimal(999);});
+	}
+}	
